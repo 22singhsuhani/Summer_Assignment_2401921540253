c7bf38ae97ff8e446a2109e7b7f4e8af9eae842a
Suhani Singh <suhani55.singh@gmail.com>
2026-08-17 22:21:36 +0530

    Time: 51 ms (66.42%) | Memory: 52.2 MB (83.67%) - LeetSync

---
+++ 384-shuffle-an-array/shuffle-an-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    int[] original;
+
+    public Solution(int[] nums) {
+        original = nums.clone();
+    }
+
+    public int[] reset() {
+        return original.clone();
+    }
+
+    public int[] shuffle() {
+        int[] arr = original.clone();
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            int j = (int)(Math.random() * (i + 1));
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        return arr;
+    }
+}
