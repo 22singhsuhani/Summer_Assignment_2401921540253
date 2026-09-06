6fafd4a503e16de09e3bf4e9e193081e737d6253
Suhani Singh <suhani55.singh@gmail.com>
2026-09-06 10:33:49 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (25.06%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -6,18 +6,25 @@ class Solution {
         int fc=0, fr=0, lc=n-1 , lr=m-1;
         int tne=m*n;
         while(fc<=lc && fr<=lr){
+            //top row
             for(int j=fc;j<=lc;j++)
                 ans.add(mat[fr][j]);
             fr++;
             if(ans.size()==tne)break;
+
+            //lc
             for(int i=fr;i<=lr;i++)
                 ans.add(mat[i][lc]);
             lc--;
             if(ans.size()==tne)break;
+
+            //lr
             for(int j=lc;j>=fc;j--)
                 ans.add(mat[lr][j]);
             lr--;
             if(ans.size()==tne)break;
+
+            //fc
             for(int i=lr;i>=fr;i--)
                 ans.add(mat[i][fc]);
             fc++;
